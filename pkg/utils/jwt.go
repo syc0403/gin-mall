94198903c36a10667b1cdd5f83dc3faa2369355f
syc0403 <652118918@qq.com>
2023-01-03 17:52:14 +0900

    用户登录完成

---
+++ pkg/utils/jwt.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+var jetSecret = []byte("FanOne")
+
+type Claims struct {
+	ID        uint   `json:"id"`
+	UserName  string `json:"user_name"`
+	Authority int    `json:"authority"`
+	jwt.StandardClaims
+}
+
+// GenerateToken 签发token
+func GenerateToken(id uint, userName string, authority int) (string, error) {
+	nowTime := time.Now()
+	expireTime := nowTime.Add(24 * time.Hour)
+	claims := Claims{
+		ID:        id,
+		UserName:  userName,
+		Authority: authority,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expireTime.Unix(),
+			Issuer:    "mall",
+		},
+	}
+	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := tokenClaims.SignedString(jetSecret)
+	return token, err
+}
